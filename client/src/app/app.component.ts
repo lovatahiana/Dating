c609061f24bc7f986ee5a6b5427d0dcb58463267
lovatahiana <lovatahianar@gmail.com>
2023-01-13 23:18:43 +0300

    Making an http request in angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5050/api/Users').subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
